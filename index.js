0ba6c3a3594ab23e50b348ce62f18805f5ba03e2
hprxj15 <henriquepereirarosa303@gmail.com>
2025-03-28 10:55:24 -0300

    not end

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+require('dotenv').config();
+const express = require('express'); 
+const cors = require('cors');
 
-const app = express();
 
-const porta = 3333;
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
 
-app.listen(porta ,() =>{
-    console.log(`servidor iniciado na porta ${porta}`);
-})  
+const porta = process.env.PORT || 3333;
+
+app.listen(porta, () => {
+    console.log(`Servidor iniciado em http://localhost:${porta}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('Hello World');
+});
+
