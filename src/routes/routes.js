002c2e348b8c61549628e465d92968787e100f98
hprxj15 <henriquepereirarosa303@gmail.com>
2025-04-14 08:56:48 -0300

    nodemon

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router(); 
 
 const LivrosController = require('../controllers/livros'); 
-const LivrosController = require('../controllers/resenhas'); 
+const ResenhasController = require('../controllers/resenhas'); 
 
-router.get('/usuarios', LivrosController.listarLivros); 
-router.post('/usuarios', LivrosController.cadastrarLivros); 
-router.patch('/usuarios', LivrosController.editarLivros); 
-router.delete('/usuarios', LivrosController.apagarLivros); 
+router.get('/livros', LivrosController.listarLivros); 
+router.post('/livros', LivrosController.cadastrarLivros); 
+router.patch('/livros', LivrosController.editarLivros); 
+router.delete('/livros', LivrosController.apagarLivros); 
 
-router.get('/usuarios', LivrosController.listarResenhas); 
-router.post('/usuarios', LivrosController.cadastrarResenhas); 
-router.patch('/usuarios', LivrosController.editarResenhas); 
-router.delete('/usuarios', LivrosController.apagarREsenhas); 
+router.get('/resenhas', ResenhasController.listarResenhas); 
+router.post('/resenhas', ResenhasController.cadastrarResenhas); 
+router.patch('/resenhas', ResenhasController.editarResenhas); 
+router.delete('/resenhas', ResenhasController.apagarResenhas); 
 
 module.exports = router;
