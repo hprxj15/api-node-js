d7e318d34b8346303af55d0ea6a0e413c3ac21b4
hprxj15 <henriquepereirarosa303@gmail.com>
2025-05-12 09:01:04 -0300

    muitos erros

--- src/controllers/resenhas.js
+++ src/controllers/resenhas.js
@@ -3,7 +3,7 @@ const db = require('../dataBase/connection');
 module.exports = {
     async listarResenhas(request, response) {
         try {
-            const {res_id, livro_id, titulo, texto, status, avaliacao} = request.bory;
+            const {res_id, livro_id, titulo, texto, status, avaliacao} = request.body;
             
             const sql = `
                 INSERT INTO RESENHAS 
@@ -44,10 +44,35 @@ module.exports = {
     }, 
     async cadastrarResenhas(request, response) {
         try {
+            //Parametros recebidos pelo corpo da requisição
+            const {res_id, livro_id, titulo, texto, status, avaliacao} = request.body;
+            //parametros recebidos pera URL via params ex: /usuario/1
+            const {id} = request.params;
+            //introduçaõ sql
+            const sql = `
+                UPDATE RESENHAS SET
+                    (livro_id = ?, resenha_titulo = ?, resenha_texto = ?, resenha_status = ?, resenha_avaliacao = ?)
+                WHERE
+                    res_id = ?
+            `;
+
+            // preparo do array com dados que serão atualizados
+            const values = [res_id, livro_id, titulo, texto, status, avaliacao];
+            // execução e obtenção de confirmação da atualização realizada
+            const [result] =  await db.query( sql, values);
+
+            if (result.affectedRows === 0) {
+                return response.status(404).json({
+                    sucesso:false,
+                    mensagem: ` usuario ${id} não encontrado!`,
+                    dados: null
+                })
+            }
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Cadastro de resenhas', 
-                dados: null
+                dados: dados
             });
         } catch (error) {
             return response.status(500).json({
@@ -62,7 +87,7 @@ module.exports = {
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Alteração no cadastro de resenhas', 
-                dados: null
+                dados: dados
             });
         } catch (error) {
             return response.status(500).json({
