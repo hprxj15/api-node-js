afe53197e62bf68349132d432dd0fc76dadf1bc0
hprxj15 <henriquepereirarosa303@gmail.com>
2025-06-03 10:34:53 -0300

    mudanças

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,8 @@
 const express = require('express'); 
 const router = express.Router(); 
 
-const LivrosController = require('../controllers/livros'); 
-const ResenhasController = require('../controllers/resenhas'); 
+const RotasHenrique= require ('./routes henrique')
 
-router.get('/livros', LivrosController.listarLivros); 
-router.post('/livros', LivrosController.cadastrarLivros); 
-router.patch('/livros/:id', LivrosController.editarLivros); 
-router.delete('/livros/:id', LivrosController.apagarLivros); 
-
-router.get('/resenhas', ResenhasController.listarResenhas); 
-router.post('/resenhas', ResenhasController.cadastrarResenhas); 
-router.patch('/resenhas/:id', ResenhasController.editarResenhas); 
-router.delete('/resenhas/:id', ResenhasController.apagarResenhas); 
+router.use('./',RotasHenrique);
 
 module.exports = router;
